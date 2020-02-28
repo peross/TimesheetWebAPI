841ccce516dfa83a1fe1948fbe2afe1fe478af4f
peross <pero.soldat@gmail.com>
2020-02-28 10:57:29 +0100

    Created timesheet model class. Created controllers in web API project. Added connection string in web config. Implement GET method in web API.

---
+++ TimesheetWebAPI/Controllers/TimesheetController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TimesheetWebAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace TimesheetWebAPI.Controllers
+{
+    public class TimesheetController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable dt = new DataTable();
+
+            string query = @"select TitleId, TitleName, Hours, 
+                            convert(varchar(10), Date, 104) as Date from tblTimesheet";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["TimesheetDb"].ConnectionString))
+            using(var cmd = new SqlCommand(query, con))
+            using(var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(dt);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, dt);
+        }
+    }
+}
---
+++ TimesheetWebAPI/Models/Timesheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimesheetWebAPI.Models
+{
+    public class Timesheet
+    {
+        public int TitleId { get; set; }
+        public string TitleName { get; set; }
+        public string Hours { get; set; }
+        public DateTime? Date { get; set; }
+
+    }
+}
